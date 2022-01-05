a8f48272a2391542028ade21d48e39a7b0236a68
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-01-05 10:03:29 +0100

    css-et-javascript-avancé

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -7,54 +7,49 @@ function bases() {
     return animation.play;
 }
 
-function carrousel8Images() {
-
-    let images = document.querySelectorAll('.image15');
+function carrouselImages() {
+    var images = document.querySelectorAll('.image');
     let d = 2000;
     let delta = 1000;
 
-    images.forEach(function(img,ind) {
-        img.style.zIndex = images.length - ind;
+    images.forEach(function(img,indice) {
+        img.style.zIndex = images.length - indice;
     });
-    
-    let anim15 = anime.timeline({
-        targets: '.imgalt',
-        loop: true,
-        direction: 'alternate',
-        easing: 'linear',
-        delay: function(img, ind) {
-            if (ind == 0) return delta;
-            if (ind == 1) return 2*delta + d;
-            return 3*delta + 2*d;
-        }
-    });
-    anim15.add({
-        translateX: [{value: 500, duration: d, easing: 'easeInOutSine'}, 
-                     {value: 500, duration: 0, delay: delta}]
-    });
-}
 
-    animC.add({
+    let anim155 = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    anim155.add({
         targets: images,
-        translateX: [{value: 500, duration: d},
-                     {value: '-500', durataion: 0, delay: function(img,ind) {
-                                        if (ind == 0) return 3*delta + 2*d;
-                                        if (ind == 1) return 2*delta + d;
-                                        if (ind == 2) return delta;
-                                        return 0;
-                                    }
-                     },
-                     {value: 0, duration: function(img,ind) {
-                                    if (ind == 3) return 0;
-                                    return d;
-                                }
-                     }],
-        easing: 'easeInOutSine',
+        translateX: [{value: '500', duration: d},
+                     {value: '-500', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (8*delta)+(7*d);
+                         if (ind == 1) return (7*delta)+(6*d);
+                         if (ind == 2) return (6*delta)+(5*d);
+                         if (ind == 3) return (5*delta)+(4*d);
+                         if (ind == 4) return (4*delta)+(3*d);
+                         if (ind == 5) return (3*delta)+(2*d);
+                         if (ind == 6) return (2*delta)+d;
+                         if (ind == 7) return delta;
+                         return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 4) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
         delay: function(img,ind) {
             if (ind == 0) return delta;
-            if (ind == 1) return 2*delta + d;
-            if (ind == 2) return 3*delta + 2*d;
-            return 4*delta + 3*d;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            if (ind == 3) return (4*delta)+(3*d);
+            if (ind == 4) return (5*delta)+(4*d);
+            if (ind == 5) return (6*delta)+(5*d);
+            if (ind == 6) return (7*delta)+(6*d);
+            if (ind == 7) return (8*delta)+(7*d);
+            return (9*delta)+(8*d);
         }
     });
 }
